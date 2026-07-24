a92747379d8b1c587b7510d7817d62f06948a846
devendrakumar92717-arch <devendrakumar92717@gmail.com>
2026-07-24 17:23:33 +0530

    Add movie and latestMovies arrays to movies.js

--- movies.js
+++ movies.js
@@ -1,1 +1,44 @@
-
+const movies = [
+    {
+        title: "KGF Chapter 2",
+        image: "images/movies/kgf2.jpg",
+        category: "Action, Drama"
+    },
+    {
+        title: "Salaar",
+        image: "images/movies/salaar.jpg",
+        category: "Action, Thriller"
+    },
+    {
+        title: "Pathaan",
+        image: "images/movies/pathaan.jpg",
+        category: "Action, Thriller"
+    },
+    {
+        title: "Pushpa",
+        image: "images/movies/pushpa.jpg",
+        category: "Action, Drama"
+    }
+];
+const latestMovies = [
+    {
+        title: "KGF Chapter 2",
+        image: "images/latest/movie1.jpg",
+        category: "Action, Drama"
+    },
+    {
+        title: "Salaar",
+        image: "images/latest/movie2.jpg",
+        category: "Action, Thriller"
+    },
+    {
+        title: "Pathaan",
+        image: "images/latest/movie3.jpg",
+        category: "Action, Thriller"
+    },
+    {
+        title: "Pushpa",
+        image: "images/latest/movie4.jpg",
+        category: "Action, Drama"
+    }
+];
