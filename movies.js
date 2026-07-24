11f2d2bb54e99dab96c0bee368a457f99edf9b7e
devendrakumar92717-arch <devendrakumar92717@gmail.com>
2026-07-24 22:05:31 +0530

    Add new movies to the movies array
    Added new movies to the existing movies array.

--- movies.js
+++ movies.js
@@ -1,44 +1,28 @@
-const movies = [
-    {
-        title: "KGF Chapter 2",
-        image: "images/movies/kgf2.jpg",
-        category: "Action, Drama"
-    },
-    {
-        title: "Salaar",
-        image: "images/movies/salaar.jpg",
-        category: "Action, Thriller"
-    },
-    {
-        title: "Pathaan",
-        image: "images/movies/pathaan.jpg",
-        category: "Action, Thriller"
-    },
-    {
-        title: "Pushpa",
-        image: "images/movies/pushpa.jpg",
-        category: "Action, Drama"
-    }
-];
-const latestMovies = [
-    {
-        title: "KGF Chapter 2",
-        image: "images/latest/movie1.jpg",
-        category: "Action, Drama"
-    },
-    {
-        title: "Salaar",
-        image: "images/latest/movie2.jpg",
-        category: "Action, Thriller"
-    },
-    {
-        title: "Pathaan",
-        image: "images/latest/movie3.jpg",
-        category: "Action, Thriller"
-    },
-    {
-        title: "Pushpa",
-        image: "images/latest/movie4.jpg",
-        category: "Action, Drama"
-    }
-];
+movies.push({
+    title: "Salaar",
+    category: "Action, Thriller",
+    image: "images/latest/movie2.jpg"
+});
+
+movies.push({
+    title: "Pathaan",
+    category: "Action, Thriller",
+    image: "images/latest/movie3.jpg"
+});
+
+movies.push({
+    title: "Pushpa",
+    category: "Action, Drama",
+    image: "images/latest/movie4.jpg"
+});
+
+movies.push({
+    title: "Animal",
+    category: "Action, Crime",
+    image: "images/latest/movie5.jpg"
+});
+// ==========================
+// STEP 25 - MOVIE DATA TEST
+// ==========================
+
+console.log(movies);
