b5039e2c9ffee9b668e70da740e1a43e40df4e33
devendrakumar92717-arch <devendrakumar92717@gmail.com>
2026-07-25 09:19:01 +0530

    Add new movie entries to movies.js
    Added multiple movies to the movies array with details including title, image, category, language, year, sizes, and download links.

--- movies.js
+++ movies.js
@@ -93,8 +93,488 @@ movies.push({
     download: "movies/animal.mp4"
 
 });
+
+movies.push({
+
+    title: "Leo",
+
+    image: "images/latest/movie6.jpg",
+
+    category: "Action, Thriller",
+
+    language: "Hindi",
+
+    year: "2023",
+
+    size720: "1.0 GB",
+
+    size1080: "2.3 GB",
+
+    size1440: "3.9 GB",
+
+    size2160: "6.0 GB",
+
+    download: "movies/leo.mp4"
+
+});
+
+movies.push({
+
+    title: "Jawan",
+
+    image: "images/latest/movie7.jpg",
+
+    category: "Action, Thriller",
+
+    language: "Hindi",
+
+    year: "2023",
+
+    size720: "980 MB",
+
+    size1080: "2.2 GB",
+
+    size1440: "3.8 GB",
+
+    size2160: "5.8 GB",
+
+    download: "movies/jawan.mp4"
+
+});
+
+movies.push({
+
+    title: "Tiger 3",
+
+    image: "images/latest/movie8.jpg",
+
+    category: "Action, Spy",
+
+    language: "Hindi",
+
+    year: "2023",
+
+    size720: "1.1 GB",
+
+    size1080: "2.4 GB",
+
+    size1440: "4.1 GB",
+
+    size2160: "6.3 GB",
+
+    download: "movies/tiger3.mp4"
+
+});
+
+movies.push({
+
+    title: "War",
+
+    image: "images/latest/movie9.jpg",
+
+    category: "Action",
+
+    language: "Hindi",
+
+    year: "2019",
+
+    size720: "900 MB",
+
+    size1080: "2.0 GB",
+
+    size1440: "3.5 GB",
+
+    size2160: "5.4 GB",
+
+    download: "movies/war.mp4"
+
+});
+
+movies.push({
+
+    title: "RRR",
+
+    image: "images/latest/movie10.jpg",
+
+    category: "Action, Drama",
+
+    language: "Hindi",
+
+    year: "2022",
+
+    size720: "1.2 GB",
+
+    size1080: "2.6 GB",
+
+    size1440: "4.3 GB",
+
+    size2160: "6.7 GB",
+
+    download: "movies/rrr.mp4"
+
+});
+
 // ==========================
 // STEP 25 - MOVIE DATA TEST
 // ==========================
 
+movies.push({
+
+    title: "Leo",
+
+    image: "images/latest/movie6.jpg",
+
+    category: "Action, Thriller",
+
+    language: "Hindi",
+
+    year: "2023",
+
+    size720: "1.0 GB",
+
+    size1080: "2.3 GB",
+
+    size1440: "3.9 GB",
+
+    size2160: "6.0 GB",
+
+    download: "movies/leo.mp4"
+
+});
+
+movies.push({
+
+    title: "Jawan",
+
+    image: "images/latest/movie7.jpg",
+
+    category: "Action, Thriller",
+
+    language: "Hindi",
+
+    year: "2023",
+
+    size720: "980 MB",
+
+    size1080: "2.2 GB",
+
+    size1440: "3.8 GB",
+
+    size2160: "5.8 GB",
+
+    download: "movies/jawan.mp4"
+
+});
+
+movies.push({
+
+    title: "Tiger 3",
+
+    image: "images/latest/movie8.jpg",
+
+    category: "Action, Spy",
+
+    language: "Hindi",
+
+    year: "2023",
+
+    size720: "1.1 GB",
+
+    size1080: "2.4 GB",
+
+    size1440: "4.1 GB",
+
+    size2160: "6.3 GB",
+
+    download: "movies/tiger3.mp4"
+
+});
+
+movies.push({
+
+    title: "War",
+
+    image: "images/latest/movie9.jpg",
+
+    category: "Action",
+
+    language: "Hindi",
+
+    year: "2019",
+
+    size720: "900 MB",
+
+    size1080: "2.0 GB",
+
+    size1440: "3.5 GB",
+
+    size2160: "5.4 GB",
+
+    download: "movies/war.mp4"
+
+});
+
+movies.push({
+
+    title: "RRR",
+
+    image: "images/latest/movie10.jpg",
+
+    category: "Action, Drama",
+
+    language: "Hindi",
+
+    year: "2022",
+
+    size720: "1.2 GB",
+
+    size1080: "2.6 GB",
+
+    size1440: "4.3 GB",
+
+    size2160: "6.7 GB",
+
+    download: "movies/rrr.mp4"
+
+});
+
+movies.push({
+
+    title: "Vikram",
+
+    image: "images/latest/movie11.jpg",
+
+    category: "Action, Thriller",
+
+    language: "Hindi",
+
+    year: "2022",
+
+    size720: "1.0 GB",
+
+    size1080: "2.3 GB",
+
+    size1440: "4.0 GB",
+
+    size2160: "6.1 GB",
+
+    download: "movies/vikram.mp4"
+
+});
+
+movies.push({
+
+    title: "Jailer",
+
+    image: "images/latest/movie12.jpg",
+
+    category: "Action, Crime",
+
+    language: "Hindi",
+
+    year: "2023",
+
+    size720: "1.1 GB",
+
+    size1080: "2.4 GB",
+
+    size1440: "4.2 GB",
+
+    size2160: "6.4 GB",
+
+    download: "movies/jailer.mp4"
+
+});
+
+movies.push({
+
+    title: "Kantara",
+
+    image: "images/latest/movie13.jpg",
+
+    category: "Action, Drama",
+
+    language: "Hindi",
+
+    year: "2022",
+
+    size720: "950 MB",
+
+    size1080: "2.1 GB",
+
+    size1440: "3.8 GB",
+
+    size2160: "5.9 GB",
+
+    download: "movies/kantara.mp4"
+
+});
+
+movies.push({
+
+    title: "Kaithi",
+
+    image: "images/latest/movie14.jpg",
+
+    category: "Action, Thriller",
+
+    language: "Hindi",
+
+    year: "2019",
+
+    size720: "900 MB",
+
+    size1080: "2.0 GB",
+
+    size1440: "3.6 GB",
+
+    size2160: "5.5 GB",
+
+    download: "movies/kaithi.mp4"
+
+});
+
+movies.push({
+
+    title: "Master",
+
+    image: "images/latest/movie15.jpg",
+
+    category: "Action, Drama",
+
+    language: "Hindi",
+
+    year: "2021",
+
+    size720: "1.0 GB",
+
+    size1080: "2.2 GB",
+
+    size1440: "3.9 GB",
+
+    size2160: "6.0 GB",
+
+    download: "movies/master.mp4"
+
+});
+
+movies.push({
+
+    title: "Pushpa 2",
+
+    image: "images/latest/movie16.jpg",
+
+    category: "Action, Drama",
+
+    language: "Hindi",
+
+    year: "2024",
+
+    size720: "1.3 GB",
+
+    size1080: "2.8 GB",
+
+    size1440: "4.6 GB",
+
+    size2160: "7.1 GB",
+
+    download: "movies/pushpa2.mp4"
+
+});
+
+movies.push({
+
+    title: "Devara",
+
+    image: "images/latest/movie17.jpg",
+
+    category: "Action, Drama",
+
+    language: "Hindi",
+
+    year: "2024",
+
+    size720: "1.2 GB",
+
+    size1080: "2.5 GB",
+
+    size1440: "4.3 GB",
+
+    size2160: "6.8 GB",
+
+    download: "movies/devara.mp4"
+
+});
+
+movies.push({
+
+    title: "Kalki 2898 AD",
+
+    image: "images/latest/movie18.jpg",
+
+    category: "Sci-Fi, Action",
+
+    language: "Hindi",
+
+    year: "2024",
+
+    size720: "1.4 GB",
+
+    size1080: "3.0 GB",
+
+    size1440: "4.9 GB",
+
+    size2160: "7.5 GB",
+
+    download: "movies/kalki2898ad.mp4"
+
+});
+
+movies.push({
+
+    title: "Singham Again",
+
+    image: "images/latest/movie19.jpg",
+
+    category: "Action",
+
+    language: "Hindi",
+
+    year: "2024",
+
+    size720: "1.2 GB",
+
+    size1080: "2.6 GB",
+
+    size1440: "4.4 GB",
+
+    size2160: "6.9 GB",
+
+    download: "movies/singhamagain.mp4"
+
+});
+
+movies.push({
+
+    title: "Stree 2",
+
+    image: "images/latest/movie20.jpg",
+
+    category: "Horror, Comedy",
+
+    language: "Hindi",
+
+    year: "2024",
+
+    size720: "950 MB",
+
+    size1080: "2.1 GB",
+
+    size1440: "3.8 GB",
+
+    size2160: "5.9 GB",
+
+    download: "movies/stree2.mp4"
+
+});
 console.log(movies);
