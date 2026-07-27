ff2cb5a702bc9699537e7381e35b52491aff9f0a
devendrakumar92717-arch <devendrakumar92717@gmail.com>
2026-07-27 18:18:08 +0530

    Fix formatting and add missing movie entries

--- movies.js
+++ movies.js
@@ -4,14 +4,22 @@ const trendingMovies = [
 title:"KGF Chapter 2",
 category:"Action, Drama",
 language:"Hindi",
-image:"images/movies/kgf2.jpg"
+image:"images/movies/kgf2.jpg",
+
+normalHD:"https://example.com/kgf720",
+fullHD:"https://example.com/kgf1080"
+
 },
 
 {
 title:"Salaar",
 category:"Action, Thriller",
 language:"Hindi",
-image:"images/movies/salaar.jpg"
+image:"images/movies/salaar.jpg",
+
+normalHD:"https://example.com/salaar720",
+fullHD:"https://example.com/salaar1080"
+
 },
 
 {
@@ -19,6 +27,9 @@ title:"Pathaan",
 category:"Action, Thriller",
 language:"Hindi",
 image:"images/movies/pathaan.jpg"
+    
+normalHD:"https://example.com/salaar720",
+fullHD:"https://example.com/salaar1080"    
 },
 
 {
@@ -26,6 +37,9 @@ title:"Brahmastra",
 category:"Adventure, Fantasy",
 language:"Hindi",
 image:"images/movies/brahmastra.jpg"
+
+normalHD:"https://example.com/salaar720",
+fullHD:"https://example.com/salaar1080"
 }
 
 ];
@@ -39,6 +53,9 @@ title:"Animal",
 category:"Action, Crime",
 language:"Hindi",
 image:"images/latest/movie1.jpg"
+
+normalHD:"https://example.com/salaar720",
+fullHD:"https://example.com/salaar1080"
 },
 
 {
@@ -46,6 +63,9 @@ title:"Jawan",
 category:"Action, Drama",
 language:"Hindi",
 image:"images/latest/movie2.jpg"
+
+normalHD:"https://example.com/salaar720",
+fullHD:"https://example.com/salaar1080"
 },
 
 {
@@ -53,6 +73,9 @@ title:"Leo",
 category:"Action, Thriller",
 language:"Hindi",
 image:"images/latest/movie3.jpg"
+
+normalHD:"https://example.com/salaar720",
+fullHD:"https://example.com/salaar1080"
 },
 
 {
@@ -60,6 +83,9 @@ title:"Tiger 3",
 category:"Action, Spy",
 language:"Hindi",
 image:"images/latest/movie4.jpg"
+
+normalHD:"https://example.com/salaar720",
+fullHD:"https://example.com/salaar1080"
 }
 
 ];
