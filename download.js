893378d292821c55f80e0c5fccbba69328be3b62
Devendra kumar <devendrakumar92717@gmail.com>
2026-08-01 22:34:48 +0530

    Remove alert for movie ID in download.js
    Remove alert for movie ID.

--- download.js
+++ download.js
@@ -7,7 +7,6 @@
 const params = new URLSearchParams(window.location.search);
 
 const movieId = Number(params.get("id"));
-alert("Movie ID = " + movieId);
 
 // ==========================================
 // FIND MOVIE
